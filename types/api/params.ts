2a27505b6ce1be76d44ce9a16b0a1f2e63c45afd
Wallace Palmer <darthdegen@gmail.com>
2026-01-26 09:04:30 -0700

    chore: added comments to types/api/params

--- types/api/params.ts
+++ types/api/params.ts
@@ -1,5 +1,7 @@
+// Union Heilus API methods used
 export type HeliusMethodName = "getAssetsByOwner";
 
+// API params for GetAssets
 export type GetAssetsByOwnerParams = {
   ownerAddress: string;
   page?: number;
